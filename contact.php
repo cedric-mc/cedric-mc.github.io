cbc55e349ed31508895451eacb4206097fcce06d
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-22 14:28:52 +0100

    Mise à jour du fichier contact.php avec les modifications suivantes : ajout des fichiers PHPMailer, mise à jour des configurations SMTP, mise à jour des informations d'authentification SMTP, ajout de l'adresse email de l'expéditeur, mise à jour du contenu de l'email.

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-    include_once("conf.php");
+    // Inclure les fichiers PHPMailer
     require_once("PHPMailer/src/PHPMailer.php");
     require_once("PHPMailer/src/SMTP.php");
     require_once("PHPMailer/src/Exception.php");
@@ -15,24 +15,25 @@
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
 
+        include_once("conf.php");
+
         $mail = new PHPMailer(true);
         // Configurations SMTP
         $mail->isSMTP(); // Envoyer en utilisant SMTP
-        $mail->Host = $emailHost; // Définir le serveur SMTP à utiliser
-        $mail->Port = $emailPort; // Port TCP à connecter
+        $mail->Host = $host; // Définir le serveur SMTP à utiliser
+        $mail->Port = 465; // Port TCP à connecter
+        $mail->SMTPSecure = "ssl";
         $mail->SMTPAuth = true; // Activer l'authentification SMTP
-        $mail->Username = $emailAddress; // Nom d'utilisateur SMTP
-        $mail->Password = $emailPassword; // Mot de passe SMTP
-
-        // Destinataires
-        $mail->setFrom($email, $nom);
-        $mail->addAddress($emailAddress, $nom); // Ajouter un destinataire
+        $mail->Username = $username; // Nom d'utilisateur SMTP
+        $mail->Password = $password; // Mot de passe SMTP
+        $mail->CharSet = "UTF-8";
+        $mail->setFrom($email, $nom); // Adresse email de l'expéditeur
 
         // Contenu
         $mail->isHTML(true); // Définir le format d'email sur HTML
         $mail->Subject = "Nouveau message du formulaire de contact du Portfolio";
         $mail->Body = "Nom: $nom\nEmail: $email\nMessage: $message";
-        $mail->CharSet = "UTF-8";
+        $mail->addAddress($emailAddress, $nom); // Ajouter un destinataire
 
         try {
             $mail->send();
