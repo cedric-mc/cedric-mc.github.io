18aff8a6046268f7a2e9eb1ea5b956c1d8e9ec9d
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-22 13:24:15 +0100

    Mise à jour du style et des classes de fond

---
+++ modals.php
@@ -0,0 +1,51 @@
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Modals</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <div class="modal fade" id="realiserN2Modal" tabindex="-1" aria-labelledby="realiserN2ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content bg-light text-dark">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="realiserN2ModalLabel">Niveau 2</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Partir des exigences et aller jusqu'à une application complète.
+                        <br>
+                        <ul>
+                            <li>Élaborer et implémenter les spécifications fonctionnelles et non fonctionnelles à partir des exigences.</li>
+                            <li>Appliquer des principes d'accessibilité et d'ergonomie.</li>
+                            <li>Adopter de bonnes pratiques de conception et de programmation.</li>
+                            <li>Vérifier et valider la qualité de l'application par les tests.</li>
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="conduireModal" tabindex="-1" aria-labelledby="conduireModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content bg-light text-dark">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="conduireModalLabel">Niveau 2</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
