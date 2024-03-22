ec68bb48ffa47cf1fcfb0827cb5180e2ce23ed97
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-22 14:12:28 +0100

    Mise à jour du formulaire de contact***

--- index.php
+++ index.php
@@ -722,15 +722,15 @@
                         <form action="contact.php" method="POST">
                             <div class="mb-3">
                                 <label for="name" class="form-label">Nom</label>
-                                <input type="text" class="form-control" name="nom" id="name" placeholder="Votre nom" required="">
+                                <input type="text" class="form-control" name="nom" id="name" placeholder="Votre nom" required>
                             </div>
                             <div class="mb-3">
                                 <label for="email" class="form-label">Adresse e-mail</label>
-                                <input type="email" class="form-control" name="email" id="email" placeholder="exemple@domaine.com" required="">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="exemple@domaine.com" required>
                             </div>
                             <div class="mb-3">
                                 <label for="message" class="form-label">Message</label>
-                                <textarea class="form-control" name="message" id="message" rows="5" placeholder="Votre message" required=""></textarea>
+                                <textarea class="form-control" name="message" id="message" rows="5" placeholder="Votre message" required></textarea>
                             </div>
                             <button type="submit" class="btn btn-primary">Envoyer</button>
                         </form>
