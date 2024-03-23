5ca3cfc4f128ceea2bf2631272295692773bb2cd
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-23 10:51:19 +0100

    Ajout des modals pour les niveaux 2 dans le fichier modals.php

--- modals.php
+++ modals.php
@@ -31,15 +31,112 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" id="optimiserN2Modal" tabindex="-1" aria-labelledby="optimiserN2ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content bg-light text-dark">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="optimiserN2ModalLabel">Optimiser - Niveau 2 : Détails</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Sélectionner les algorithmes adéquats pour répondre à un problème donné.
+                        <br>
+                        <ul>
+                            <li>Choisir des structures de données complexes adaptées au problème.</li>
+                            <li>Utiliser des techniques algorithmiques adaptées pour des problèmes complexes (par ex. recherche opérationnelle, méthodes arborescentes, optimisation globale, intelligence artificielle ... ).</li>
+                            <li>Comprendre les enjeux et moyens de sécurisation des données et du code.</li>
+                            <li>Évaluer l'impact environnemental et sociétal des solutions proposées.</li>
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="administrerN2Modal" tabindex="-1" aria-labelledby="administrerN2ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content bg-light text-dark">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="administrerN2ModalLabel">Optimiser - Niveau 2 : Détails</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Déployer des services dans une architecture réseau.
+                        <br>
+                        <ul>
+                            <li>Concevoir et développer des applications communicantes.</li>
+                            <li>Utiliser des serveurs et des services réseaux virtualisés.</li>
+                            <li>Sécuriser les services et données d'un système.</li>
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="gererN2Modal" tabindex="-1" aria-labelledby="gererN2ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content bg-light text-dark">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="gererN2ModalLabel">Optimiser - Niveau 2 : Détails</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Optimiser une base de données, intéragir avec une application et mettre en œuvre des procédures la sécurité.
+                        <br>
+                        <ul>
+                            <li>Concevoir et développer des applications communicantes.</li>
+                            <li>Utiliser des serveurs et des services réseaux virtualisés.</li>
+                            <li>Sécuriser les services et données d'un système.</li>
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="modal fade" id="conduireN2Modal" tabindex="-1" aria-labelledby="conduireN2ModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content bg-light text-dark">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="conduireN2ModalLabel">Niveau 2</h1>
+                        <h1 class="modal-title fs-5" id="conduireN2ModalLabel">Conduire - Niveau 2 : Détails</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-
+                        Appliquer une démarche de suivi de projet en fonction des besoins métiers des clients et des utilisateurs.
+                        <br>
+                        <ul>
+                            <li>Identifier les processus présents dans une organisation en vue d'améliorer les systèmes d'information.</li>
+                            <li>Formaliser les besoins du client et de l'utilisateur.</li>
+                            <li>Identifier les critères de faisabilité d'un projet informatique.</li>
+                            <li>Définir et mettre en œuvre une démarche de suivi de projet.</li>
+                        </ul>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="collaborerN2Modal" tabindex="-1" aria-labelledby="collaborerN2ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content bg-light text-dark">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="collaborerN2ModalLabel">Collaborer - Niveau 2 : Détails</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Situer son rôle et ses missions au sein d'une équipe informatique
+                        <br>
+                        <ul>
+                            <li>Comprendre la diversité, la structure et la dimension de l'informatique dans une organisation (ESN, DSI, ... ).</li>
+                            <li>Appliquer une démarche pour intégrer une équipe informatique au sein d'une organisation.</li>
+                            <li>Mobiliser les compétences interpersonnelles pour travailler dans une équipe informatique.</li>
+                            <li>Rendre compte de son activité professionnelle.</li>
+                        </ul>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
