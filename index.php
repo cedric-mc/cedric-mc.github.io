8aceccb2c416510ac294483e0b784298d6bab256
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-25 09:37:45 +0100

    Mise à jour de la fonction de validation des formulaires***

--- index.php
+++ index.php
@@ -207,6 +207,7 @@
                                 </div>
                                 <div class="card-footer text-end text-body-secondary">
                                     <a href="#batailleBoules" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Bataille des Boules</a>
+                                    <a href="#semonkey" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Semonkey</a>
                                 </div>
                             </div>
                         </div>
@@ -230,6 +231,7 @@
                                 </div>
                                 <div class="card-footer text-end text-body-secondary">
                                     <a href="#batailleBoules" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Bataille des Boules</a>
+                                    <a href="#semonkey" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Semonkey</a>
                                 </div>
                             </div>
                         </div>
@@ -252,6 +254,7 @@
                                     </div>
                                 </div>
                                 <div class="card-footer text-end text-body-secondary">
+                                    <a href="#semonkey" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Semonkey</a>
                                 </div>
                             </div>
                         </div>
@@ -276,6 +279,7 @@
                                 <div class="card-footer text-end text-body-secondary">
                                     <a href="#banklink" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Banklink</a>
                                     <a href="#hedelma" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Hedelma</a>
+                                    <a href="#semonkey" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Semonkey</a>
                                     <a href="#cnil" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Contrôles CNIL</a>
                                 </div>
                             </div>
@@ -301,6 +305,7 @@
                                 <div class="card-footer text-end text-body-secondary">
                                     <a href="#banklink" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Banklink</a>
                                     <a href="#toledo" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Toledo</a>
+                                    <a href="#semonkey" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Semonkey</a>
                                 </div>
                             </div>
                         </div>
@@ -325,6 +330,7 @@
                                 <div class="card-footer text-end text-body-secondary">
                                     <a href="#banklink" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Banklink</a>
                                     <a href="#toledo" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Toledo</a>
+                                    <a href="#semonkey" class="card-link link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Semonkey</a>
                                 </div>
                             </div>
                         </div>
@@ -341,7 +347,7 @@
                 <div class="projet-container" id="semonkey">
                     <div class="project-item">
                         <h3 class="title-project">Semonkey, Jeu en ligne à un ou plusieurs joueurs</h3>
-                        <h4 class="project-title">HTML, CSS, PHP, SQL, JS, C, Java, TypeScript, React</h4>
+                        <h4 class="project-title">HTML&ensp;<i class="fa-brands fa-html5 character"></i>, CSS&ensp;<i class="fa-brands fa-css3-alt character"></i>, PHP&ensp;<i class="fa-brands fa-php character"></i>, SQL&ensp;<i class="fa-solid fa-database character"></i>, JS&ensp;<i class="fa-brands fa-square-js character"></i>, C&ensp;<i class="fa-solid fa-c character"></i>, Java&ensp;<i class="fa-brands fa-java character"></i>, TypeScript&ensp;, React&ensp;<i class="fa-brands fa-react character"></i>, Android&ensp;</h4>
                         <p>
                             Semonkey est un jeu en ligne à un ou plusieurs joueurs.
                             Chaque joueur reçoit un mot de départ et un mot cible et propose des mots proches afin de créer une chaîne de mots similaires pour relier le mot de départ au mot cible.
@@ -352,6 +358,7 @@
                                 <li><a href="#chain-motor" class="link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">ChainMotor : Moteur de Chaîne</a></li>
                             </ul>-->
                             Une version mobile est en cours de développement.
+                            <br>
                             Site :&ensp;
                             <a href="http://perso-etudiant.u-pem.fr/~mariyaconsta02/Semonkey/" target="_blank">
                                 <img class="p-img" src="img/code.png" alt="Code Icon">
@@ -361,6 +368,18 @@
                             <a href="https://cedric-mc.github.io/Banklink" target="_blank">
                                 <img class="p-img" src="img/documentation.png" alt="Documentation Icon">
                             </a>-->
+                            <br>
+                            Compétence Réalisée : Ce projet m'a permis de réaliser une application web en utilisant différents langages de programmation.
+                            <br>
+                            Compétence Optimiser : Ce projet m'a permis de proposer une application web optimisée en fonction de critères spécifiques et de proposer une version mobile.
+                            <br>
+                            Compétence Administrer : Ce projet m'a permis de mettre à disposition une application web et de maintenir en conditions opérationnelles des services et des réseaux.
+                            <br>
+                            Compétence Gérer : Ce projet m'a permis de concevoir, gérer, administrer et exploiter des données.
+                            <br>
+                            Compétence Conduire : Ce projet m'a permis de satisfaire les besoins des utilisateurs et de piloter un projet informatique.
+                            <br>
+                            Compétence Collaborer : Ce projet m'a permis de travailler efficacement dans une équipe informatique.
                         </p>
                         <div id="carouselSemonkey" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-indicators">
@@ -409,6 +428,7 @@
                         <p>
                             Le projet Contrôles CNIL a pour objectif d'exploiter les données ouvertes concernant les contrôles réalisés par la CNIL de 2017 à 2022.
                             L'objectif est de valoriser ces données en les présentant sous forme d'un site web comprenant une carte et des graphiques permettant une visualisation claire et interactive des informations, afin de créer un tableau de bord informatif.
+                            <br>
                             Site :&ensp;
                             <a href="https://perso-etudiant.u-pem.fr/~mariyaconsta02/controles_cnil/" target="_blank">
                                 <img class="p-img" src="img/code.png" alt="Code Icon">
@@ -418,6 +438,8 @@
                             <a href="" target="_blank">
                                 <img class="p-img" src="img/documentation.png" alt="Documentation Icon">
                             </a>-->
+                            <br>
+                            Compétence Gérer : Ce projet m'a permis de concevoir, gérer, administrer et exploiter des données officielles d'une autorité administrative.
                         </p>
                         <div id="carouselCNIL" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-indicators">
@@ -426,6 +448,7 @@
                                 <button type="button" data-bs-target="#" data-bs-slide-to="2" aria-label="Slide 3"></button>
                                 <button type="button" data-bs-target="#" data-bs-slide-to="3" aria-label="Slide 4"></button>
                                 <button type="button" data-bs-target="#" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                                <button type="button" data-bs-target="#" data-bs-slide-to="5" aria-label="Slide 6"></button>
                             </div>
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
@@ -442,6 +465,9 @@
                                 </div>
                                 <div class="carousel-item">
                                     <img src="img/projects/CNIL5.png" class="d-block w-100" alt="CNIL 5">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="img/projects/CNIL6.png" class="d-block w-100" alt="CNIL 6">
                                 </div>
                             </div>
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselCNIL" data-bs-slide="prev">
@@ -461,6 +487,18 @@
                         <h4 class="project-title">Java&ensp;<i class="fa-brands fa-java"></i></h4>
                     </div>
                 </div>-->
+                <div class="project-container" id="reseau">
+                    <div class="project-item">
+                        <h3 class="title-project">TP Pare-feu et translation d'adresse</h3>
+                        <h4 class="project-title">Bash&ensp;<i class="fa-brands fa-linux character"></i></h4>
+                        <p>
+                            Le TP Pare-feu et translation d'adresse a pour objectif de mettre en place un pare-feu sous Linux grâce à Netfilter et mettre en place une passerelle NAT (Network Address Translation).
+                            <br>
+                            Compétence Administrer : Ce projet m'a permis de mettre à disposition un pare-feu et une translation d'adresse sur un réseau local.
+                        </p>
+                        <img src="img/projects/RESEAU1.png" class="d-block w-100" alt="Réseau">
+                    </div>
+                </div>
                 <div class="project-container" id="banklink">
                     <div class="project-item">
                         <h3 class="title-project">Banklink, Portail Web de Gestion de paiements par carte bancaire</h3>
@@ -476,6 +514,12 @@
                             <a href="https://cedric-mc.github.io/Banklink" target="_blank">
                                 <img class="p-img" src="img/documentation.png" alt="Documentation Icon">
                             </a>
+                            <br>
+                            Compétence Gérer : Ce projet m'a permis de concevoir, gérer, administrer et exploiter des données bancaires.
+                            <br>
+                            Compétence Conduire : Ce projet m'a permis de satisfaire les besoins des utilisateurs et de piloter un projet informatique.
+                            <br>
+                            Compétence Collaborer : Ce projet m'a permis de travailler efficacement dans une équipe informatique en suivant une méthodologie de développement.
                         </p>
                         <div id="carouselBanklink" class="carousel slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-indicators">
@@ -544,8 +588,13 @@
                             Site :&ensp;
                             <a href="Projects/Toledo/index.php" target="_blank">
                                 <img class="p-img" src="img/code.png" alt="Code Icon">
-                            </a><br>
+                            </a>
+                            <br>
                             <!--Documentation :&ensp; <a href="" target="_blank"><img class="p-img" src="img/documentation.png" alt="Documentation Icon"></a>-->
+                            <br>
+                            Compétence Conduire : Ce projet m'a permis de satisfaire les besoins des utilisateurs et de piloter un projet informatique.
+                            <br>
+                            Compétence Collaborer : Ce projet m'a permis de travailler efficacement dans une équipe informatique pour la première fois.
                         </p>
                         <div id="carouselToledo" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-indicators">
@@ -586,7 +635,7 @@
 
                 <div class="project-container" id="hedelma">
                     <div class="project-item">
-                        <h3 class="title-project">Projet Primeurs-Passion</h3>
+                        <h3 class="title-project">Projet Hedelma, Primeurs-Passion</h3>
                         <h4 class="project-title">HTML, CSS, JS, PHP, SQL, Base de données PostGreSQL, Looping</h4>
                         <p>
                             Ce projet consistait à réaliser un site web pour une entreprise fictive de vente de fruits et légumes.<br>
@@ -595,6 +644,8 @@
                                 <img class="p-img" src="img/code.png" alt="Code Icon">
                             </a><br>
                             Documentation :&ensp; <a href="" target="_blank"><img class="p-img" src="img/documentation.png" alt="Documentation Icon"></a>
+                            <br>
+                            Compétence Gérer : Ce projet m'a permis de concevoir, gérer, administrer et exploiter des données pour une entreprise fictive.
                         </p>
                         <div id="carouselHedelma" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-indicators">
@@ -652,7 +703,11 @@
                             Documentation :
                             <a href="https://cedric-mc.github.io/Python_Game" target="_blank">
                                 <img class="p-img" src="img/documentation.png" alt="Documentation Icon">
-                            </a><br>
+                            </a>
+                            <br>
+                            Compétence Réaliser : Ce projet m'a permis de réaliser une application avec une interface graphique en Python. J'ai appris à utiliser la bibliothèque Pygame pour la réalisation de jeux en Python.
+                            <br>
+                            Compétence Optimiser : Ce projet m'a permis d'optimiser mon code pour qu'il soit plus lisible et plus performant. J'ai appris à utiliser des fonctions pour réduire la taille de mon code et à utiliser des boucles pour répéter des actions.
                         </p>
                         <h5 class="caption-title">Jeu Bataille des Boules, programmé en Python, se joue à 2 joueurs.</h5>
                         <p>
@@ -716,7 +771,7 @@
                     </div>
                 </div>
 
-                <!--<div class="project-container">
+                <div class="project-container">
                     <div class="project-item">
                         <h3 class="title-project">Wrong number : a very embarrassing misunderstanding</h3>
                         <h4 class="project-title">Logiciel : ClipChamp</h4>
@@ -739,7 +794,7 @@
                             </iframe>
                         </p>
                     </div>
-                </div>-->
+                </div>
             </section>
         </main>
         <!-- Main -->
