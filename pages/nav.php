27310dd2fe19c20177d249f6c9c275a1f7f7f310
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2024-07-30 16:24:41 +0200

    changement pour séparer dans différents fichiers

---
+++ pages/nav.php
@@ -0,0 +1,42 @@
+<!-- Navbar, Navigation -->
+<nav class="navbar sticky-top navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#top-page">
+            <img src="img/navbar/home.png" class="d-inline-block align-text-top" alt="icon home">
+            Accueil
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav custom-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#about-me">
+                        <img src="img/navbar/about-me.png" class="d-inline-block align-text-top nav-img" alt="icon me"> Me Connaître
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#parcours">
+                        <img src="img/navbar/histoire.png" class="d-inline-block align-text-top" alt="icon parcours"> Parcours
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#skills">
+                        <img src="img/navbar/skills.png" class="d-inline-block align-text-top" alt="icon skills"> Compétences
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#projects">
+                        <img src="img/navbar/projects.png" class="d-inline-block align-text-top" alt="icon projects"> Projets
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#contact">
+                        <img src="img/navbar/contact.png" class="d-inline-block align-text-top" alt="icon contact"> Me Contacter
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Navbar, Navigation -->
