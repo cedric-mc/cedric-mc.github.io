892409cbabe12fe75d7854d0ab173d09d35fc3d9
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2024-09-05 12:20:35 +0200

    taille du texte parcours

--- pages/parcours.php
+++ pages/parcours.php
@@ -1,41 +1,68 @@
 <section id="parcours">
     <h2 class="title-section">Mon Parcours</h2>
     <h3>Mon Parcours de Formation</h3>
-    <ul class="list-group">
-        <li class="list-group-item">
-            <h4>BUT Informatique</h4>
-            <p>
-                <span class="italic-text">2022 - 2025</span><br>
-                <span class="italic-text">Université Gustave Eiffel</span><br>
-                <span class="italic-text">Champs-sur-Marne</span><br>
-                <span class="italic-text">Parcours A : Réalisation d’applications : conception, développement, validation</span>
-            </p>
-        </li>
-        <li class="list-group-item background">
-            <h4>BAC STI2D</h4>
-            <p>
-                <span class="italic-text">2020 - 2022</span><br>
-                <span class="italic-text">Lycée Gaston Bachelard</span><br>
-                <span class="italic-text">Chelles</span><br>
-                <span class="italic-text">Spécialité SIN : Système d'Information et Numérique</span>
-            </p>
-        </li>
-    </ul>
+    <br>
+    <div class="row">
+        <div class="col-4">
+            <div class="list-group" id="list-tab-formation" role="tablist">
+                <a class="list-group-item list-group-item-action list-group-item-info active" id="list-but-informatique-list" data-bs-toggle="list" href="#list-but-informatique" role="tab" aria-controls="but-informatique">BUT Informatique</a>
+                <a class="list-group-item list-group-item-action list-group-item-info" id="list-bac-sti2d-list" data-bs-toggle="list" href="#list-bac-sti2d" role="tab" aria-controls="bac-sti2d">BAC STI2D</a>
+            </div>
+        </div>
+        <div class="col-8">
+            <div class="tab-content" id="nav-tabContent-formation">
+                <div class="tab-pane fade show active" id="list-but-informatique" role="tabpanel" aria-labelledby="list-but-informatique-list">
+                    <h4>BUT Informatique</h4>
+                    <p class="italic-text">
+                        <span class="fw-bold">2022 - 2025</span><br>
+                        Université Gustave Eiffel<br>
+                        Champs-sur-Marne<br>
+                        Parcours A : Réalisation d’applications : conception, développement, validation
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="list-bac-sti2d" role="tabpanel" aria-labelledby="list-bac-sti2d-list">
+                    <h4>BAC STI2D</h4>
+                    <p class="italic-text">
+                        2020 - 2022<br>
+                        Lycée Gaston Bachelard<br>
+                        Chelles<br>
+                        Spécialité SIN : Système d'Information et Numérique
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
     <br>
     <h3>Mon Parcours Professionnel</h3>
-    <ul class="list-group">
-        <li class="list-group-item">
-            <h4>Stage de développeur web</h4>
-            <span class="italic-text">Du 17 juin au 9 août 2024</span><br>
-            <span class="italic-text">Centre de recherche de l'école des officiers de la gendarmerie nationale</span><br>
-            <span class="italic-text">Melun/Paris</span><br>
-            <span class="italic-text">Développement web applicatif pour la gendarmerie avec utilisation de données</span>
-        </li>
-        <li class="list-group-item">
-            <h4>Stage d'observation de 3<sup>ème</sup></h4>
-            <span class="italic-text">Semaine du lundi 4 au samedi 9 février 2019</span><br>
-            <span class="italic-text">Bulles Dogs</span><br>
-            <span class="italic-text">Chelles</span><br>
-            <span class="italic-text">Toilettage canin</span>
-        </li>
+    <br>
+    <div class="row">
+        <div class="col-4">
+            <div class="list-group" id="list-tab-professionnel" role="tablist">
+                <a class="list-group-item list-group-item-action list-group-item-info active" id="list-stage-dev-web-list" data-bs-toggle="list" href="#list-stage-dev-web" role="tab" aria-controls="stage-dev-web">Stage de développeur web</a>
+                <a class="list-group-item list-group-item-action list-group-item-info" id="list-stage-3eme-list" data-bs-toggle="list" href="#list-stage-3eme" role="tab" aria-controls="stage-3eme">Stage d'observation de 3<sup>ème</sup></a>
+            </div>
+        </div>
+        <div class="col-8">
+            <div class="tab-content" id="nav-tabContent-professionnel">
+                <div class="tab-pane fade show active" id="list-stage-dev-web" role="tabpanel" aria-labelledby="list-stage-dev-web-list">
+                    <h4>Stage de développeur web</h4>
+                    <p class="italic-text">
+                        Du 17 juin au 9 août 2024<br>
+                        Centre de recherche de l'école des officiers de la gendarmerie nationale<br>
+                        Melun/Paris<br>
+                        Développement web applicatif pour la gendarmerie avec utilisation de données
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="list-stage-3eme" role="tabpanel" aria-labelledby="list-stage-3eme-list">
+                    <h4>Stage d'observation de 3<sup>ème</sup></h4>
+                    <p class="italic-text">
+                        Semaine du lundi 4 au samedi 9 février 2019<br>
+                        Bulles Dogs<br>
+                        Chelles<br>
+                        Toilettage canin
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
