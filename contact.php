6f4ae79d58e64e6a500c05f0c35bd7a45d60bbc5
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2024-09-17 11:35:05 +0200

    Refactorise le code PHP pour améliorer l'envoi des emails depuis le formulaire de contact

--- contact.php
+++ contact.php
@@ -7,11 +7,8 @@
     $nom = $_POST['nom'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    // Afficher les erreurs PHP
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
 
-    include_once("config.php");
+    require_once('config.php');
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
@@ -24,25 +21,26 @@
         // Envoyer l'email
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;
         $mail->isSMTP();
-        $mail->Host       = $host;
+        $mail->Host       = MAIL_HOST;
         $mail->SMTPAuth   = true;
-        $mail->Username   = $username;
-        $mail->Password   = $password;
+        $mail->Username   = MAIL_USERNAME;
+        $mail->Password   = MAIL_PASSWORD;
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 465;
+        $mail->Port       = MAIL_PORT;
+        $mail->CharSet = 'UTF-8';
 
         // Destinataire
         $mail->setFrom($email, $nom);
-        $mail->addAddress($username, "Cédric Mariya Constantine");
+        $mail->addAddress(MAIL_USERNAME, "Cédric Mariya Constantine");
 
         // Contenu
         $mail->isHTML(true);
-        $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        $mail->Subject = MAIL_SUBJECT;
+        $mail->Body    = MAIL_BODY . "<br><br><strong>Nom</strong> : " . $nom . "<br><strong>Email</strong> : " . $email . "<br><strong>Message</strong> : " . $message;
+        $mail->AltBody = MAIL_ALT_BODY . "\n\nNom : " . $nom . "\nEmail : " . $email . "\nMessage : " . $message;
 
         $mail->send();
-        echo 'Message has been sent';
+        echo "Message has been sent";
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
