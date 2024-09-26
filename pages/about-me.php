fbfbfc769dfba741ba6c8e5f2753336694627f65
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-09-26 22:07:31 +0200

    màj du chemin relatif du CV

--- pages/about-me.php
+++ pages/about-me.php
@@ -5,7 +5,7 @@
             <p class="text-justify">
                 Je suis un étudiant à l'Université de Gustave Eiffel en 2<sup>ème</sup> année du BUT Informatique. Je suis actuellement en recherche d'une alternance dans le domaine du développement informatique pour une durée de 12 mois à partir de septembre 2024. Je suis à la recherche d'une entreprise qui me permettra de mettre en pratique mes connaissances et mes compétences dans le domaine du développement informatique.
             </p>
-            <p>Voici mon CV, il est à votre disposition :&nbsp; <a id="cv-btn" class="btn btn-danger heartbeat heartbeat-hover" href="doc/CV_CEDRIC_MARIYA CONSTANTINE.pdf" target="_blank">Télécharger&nbsp;<i class="fas fa-download"></i></a></p>
+            <p>Voici mon CV, il est à votre disposition :&nbsp; <a id="cv-btn" class="btn btn-danger heartbeat heartbeat-hover" href="doc/CV_CEDRIC_MARIYA-CONSTANTINE.pdf" target="_blank">Télécharger&nbsp;<i class="fas fa-download"></i></a></p>
             <p>
                 Je maîtrise différentes langues en dehors du français : l'Anglais <img class="p-img" src="img/navbar/eng.png" alt="icon flag english"> bien entendu et le Tamoul <img class="p-img" src="img/tamil.png" alt="icon flag tamil"> (langue maternelle).
             </p>
