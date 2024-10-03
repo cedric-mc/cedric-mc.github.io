214352418db2f90ab66b65cd49a1263997b23876
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-10-03 08:59:15 +0200

    mode debug + projets pro et univ + transition color modes + scroll-margin-top -> section

--- index.php
+++ index.php
@@ -4,16 +4,18 @@ session_start();
 // Mode debug <====================
 //$_SESSION['debug'] = true;
 // Si le mode debug est activé, on affiche les erreurs
-if ($_SESSION['debug']) {
+if (!empty($_SESSION['debug'])) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
 }
 // ====================> Mode debug
 
 require_once('config.php');
 require_once(PATH_SITE . 'includes/functions.php');
-
 //$_GET['e'] = 1; // Pour le mode études en permanence
 // On récupère le paramètre d'URL 'e' pour savoir si on doit afficher en mode études
 if (isset($_GET['e']) && $_GET['e'] == '1') {
