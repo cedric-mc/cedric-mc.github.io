2c01f0ffa3526858757c3f308a3266a1d281bae8
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-11-14 10:13:41 +0100

    suppression du répertoire Projects + màj final avant transition React

--- pages/header.php
+++ pages/header.php
@@ -2,7 +2,7 @@
     <div id="name-container">
         <h1 class="name lobster-regular">Cédric Mariya Constantine</h1>
         <div id="wrapper">
-            <h2 class="etudiant-animation yatra-one-regular">Étudiant en Informatique</h2>
+            <h2 class="etudiant-animation yatra-one-regular">Étudiant en informatique</h2>
             <div id="theme-display"></div>
         </div>
     </div>
