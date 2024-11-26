d33c9bbf77c9609db125b0d85982567d03fe72bb
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-11-26 21:43:18 +0100

    Correction de l'apparition de ScrollDown et ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,7 @@ import ScrollToTop from "./components/common/ScrollToTop";
 import ScrollDown from "./components/common/ScrollDown";
 import { Navigation, Header, AboutMe, Parcours, Skills, Projects, Footer } from "./components/sections";
 
-// Fonction pour calculer le thème basé sur l'heure actuelle
-function themeBasedOnTime() {
-    const hour = new Date().getHours();
-    // Choisir un thème clair entre 6h00 et 18h00, sinon sombre
-    return (hour >= 6 && hour < 18) ? "light" : "dark";
-}
-
-// Fonction pour mettre à jour l'attribut "data-theme" de tous les éléments sur la page
-function updateAllElementsThemeAttribute(attribute, value) {
-    const elements = document.querySelectorAll("[data-theme]");
-    elements.forEach((element) => {
-        element.setAttribute(attribute, value);
-    });
-}
-
-// Fonction pour calculer le thème à appliquer
-function calculateTheme({ storageThem }) {
-    if (storageThem && storageThem !== "auto") {
-        return storageThem;
-    }
-
-    // Si le thème est "auto", on utilise la logique basée sur l'heure
-    if (storageThem === "auto" || !storageThem) {
-        const autoTheme = themeBasedOnTime();
-        updateAllElementsThemeAttribute("data-auto-theme", autoTheme);
-        return "auto";
-    }
-
-    // Si aucune condition n'est remplie, on utilise la préférence système
-    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-}
-
 function App() {
-    // Récupération du thème de la page web depuis le localStorage
-    const storageThem = localStorage.getItem('theme');
-
-    // Calcul initial du thème actuel
-    let currentThemeSetting = calculateTheme({ storageThem });
-
-    // Appliquer le thème actuel à tous les éléments ayant "data-theme"
-    updateAllElementsThemeAttribute("data-theme", currentThemeSetting);
-
     return (
         <div id="top-page" className="background">
             <SvgDefs/>
--- src/components/common/ScrollDown.jsx
+++ src/components/common/ScrollDown.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import lottie from "lottie-web";
 
 function ScrollDown() {
+    const [isVisible, setIsVisible] = useState(true);
     useEffect(() => {
         const animationInstance = lottie.loadAnimation({
             container: document.getElementById('lottie-container'),
@@ -11,14 +12,30 @@ function ScrollDown() {
             animationData: require('../../data/animation.json')
         });
 
+        // Gestion de la visibilité du composant
+        const footer = document.getElementById('footer');
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsVisible(!entry.isIntersecting); // Cache ScrollDown si le footer est visible
+            },
+            { root: null, threshold: 0.5} // 10% de visibilité
+        );
+
+        if (footer) {
+            observer.observe(footer);
+        }
+
         // Nettoyage : détruire l'animation pour éviter les doublons
         return () => {
             animationInstance.destroy();
+            if (footer) {
+                observer.disconnect();
+            }
         };
     }, []); // Exécuter une seule fois après le montage du composant
 
     return (
-        <div id="arrow-container">
+        <div id="arrow-container" className={!isVisible ? 'hidden' : ''}>
             <div id="lottie-container"></div>
         </div>
     );
