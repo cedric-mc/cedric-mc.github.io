4a3c7a06f915381dcc4a5a2ac5a2581f8d035895
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-17 14:57:46 +0100

    update: refactor Parcours component to improve structure and add new internship details

--- src/pages/Parcours.tsx
+++ src/pages/Parcours.tsx
@@ -1,33 +1,34 @@
-import { Col, ListGroup, ListGroupItem, Row, TabContainer, TabContent, TabPane } from "react-bootstrap";
+import { Badge, Col, ListGroup, ListGroupItem, Row, Stack, TabContainer, TabContent, TabPane } from "react-bootstrap";
 import '@styles/pages/Parcours.css';
 import eiffel from '@assets/univ-gustave-eiffel.png';
 import bachelard from '@assets/gaston-bachelard.png';
 import crgn from '@assets/crgn-logo.png';
 import bullesDogs from '@assets/bulles-dogs.webp';
+import MI from '@assets/ministere_interieur.png';
 
-export function Parcours() {
+export function Parcours({ theme }: { theme: string }) {
     return (
         <section id="parcours">
-            <h2 className="title-section">Mon Parcours</h2>
+            <h2 className="title-section">Mon Parcours</h2> {/* TODO: Centrer les titres de niveau 2 */}
             <h3 className="subtitle-section">Mon Parcours de Formation</h3>
             <br/>
-            <TabContainer id="list-tab-formation" defaultActiveKey="#list-but-informatique">
+            <TabContainer id="list-tab-formation" defaultActiveKey="#list-formation-but">
                 <Row>
                     <Col sm={4}>
                         <ListGroup>
-                            <ListGroupItem action href="#list-but-informatique" variant="info">
+                            <ListGroupItem action href="#list-formation-but" variant="info">
                                 BUT Informatique
                             </ListGroupItem>
-                            <ListGroupItem action href="#list-bac-sti2d" variant="info">
+                            <ListGroupItem action href="#list-formation-bac" variant="info">
                                 BAC STI2D
                             </ListGroupItem>
                         </ListGroup>
                     </Col>
                     <Col sm={8}>
                         <TabContent>
-                            <TabPane eventKey="#list-but-informatique">
+                            <TabPane eventKey="#list-formation-but">
                                 <Row>
-                                    <Col sm={6} className="d-flex flex-column justify-content-center">
+                                    <Col className="d-flex flex-column justify-content-center">
                                         <h4 className="parcours-name">BUT Informatique</h4>
                                         <p className="fst-italic">
                                             <span className="fw-bold">2022 - 2025</span> (en cours)<br/>
@@ -36,14 +37,14 @@ export function Parcours() {
                                             Parcours A : Réalisation d’applications : conception, développement, validation
                                         </p>
                                     </Col>
-                                    <Col sm={6} className="d-flex justify-content-center">
+                                    <Col sm={4} className="d-flex justify-content-center">
                                         <img src={eiffel} alt="Université Gustave Eiffel" className="parcours-image"/>
                                     </Col>
                                 </Row>
                             </TabPane>
-                            <TabPane eventKey="#list-bac-sti2d">
+                            <TabPane eventKey="#list-formation-bac">
                                 <Row>
-                                    <Col sm={6} className="d-flex flex-column justify-content-center">
+                                    <Col className="d-flex flex-column justify-content-center">
                                         <h4 className="parcours-name">BAC STI2D</h4>
                                         <p className="fst-italic">
                                             <span className="fw-bold">2020 - 2022</span><br/>
@@ -52,7 +53,7 @@ export function Parcours() {
                                             Spécialité SIN : Système d'Information et Numérique
                                         </p>
                                     </Col>
-                                    <Col sm={6} className="d-flex justify-content-center">
+                                    <Col sm={4} className="d-flex justify-content-center">
                                         <img src={bachelard} alt="Lycée Gaston Bachelard" className="parcours-image"/>
                                     </Col>
                                 </Row>
@@ -64,16 +65,36 @@ export function Parcours() {
             <br/>
             <h3 className="subtitle-section">Mon Parcours Professionnel</h3>
             <br/>
-            <TabContainer id="list-tab-professionnel" defaultActiveKey="#list-stage-dev-web-list">
+            <TabContainer id="list-tab-professionnel" defaultActiveKey="#list-stage-but2">
                 <Row>
                     <Col sm={8}>
                         <TabContent>
-                            <TabPane eventKey="#list-stage-dev-web-list">
+                            <TabPane eventKey="#list-stage-but3">
                                 <Row>
-                                    <Col sm={6} className="d-flex justify-content-center">
+                                    <Col sm={4} className="d-flex justify-content-center">
+                                        <img src={MI} alt="Sous-bureau des cabinets" className="parcours-image"/>
+                                    </Col>
+                                    <Col className="d-flex flex-column justify-content-center">
+                                        <h4 className="parcours-name">Stage de développeur php</h4>
+                                        <p className="fst-italic">
+                                            <span className="fw-bold">Du 24 mars au 26 août 2025</span><br/>
+                                            Sous-bureau des cabinets du ministère de l'intérieur<br/>
+                                            Paris<br/>
+                                            Développement php
+                                        </p>
+                                        {/* <Stack direction='horizontal' gap={3}>
+                                            <Badge bg={theme} className='text-background'>PHP</Badge>
+                                            <Badge bg={theme} className='text-background'>SQL/MySQL</Badge>
+                                        </Stack> */}
+                                    </Col>
+                                </Row>
+                            </TabPane>
+                            <TabPane eventKey="#list-stage-but2">
+                                <Row>
+                                    <Col sm={4} className="d-flex justify-content-center">
                                         <img src={crgn} alt="CRGN" className="parcours-image"/>
                                     </Col>
-                                    <Col sm={6} className="d-flex flex-column justify-content-center">
+                                    <Col className="d-flex flex-column justify-content-center">
                                         <h4 className="parcours-name">Stage de développeur web</h4>
                                         <p className="fst-italic">
                                             <span className="fw-bold">Du 17 juin au 9 août 2024</span><br/>
@@ -82,23 +103,23 @@ export function Parcours() {
                                             Développement web applicatif pour la gendarmerie nationale avec utilisation de données
                                         </p>
                                         {/* <SkillBadge skill="HTML"/> */}
-                                        {/* <Stack direction='horizontal' gap={3}>
-                                            <Badge bg="light" className='text-background'>HTML</Badge>
-                                            <Badge bg="light" className='text-background'>CSS</Badge>
-                                            <Badge bg="light" className='text-background'>JavaScript</Badge>
-                                            <Badge bg="light" className='text-background'>PHP</Badge>
-                                            <Badge bg="light" className='text-background'>SQL/MySQL</Badge>
-                                            <Badge bg="light" className='text-background'>Python</Badge>
-                                        </Stack> */}
+                                        <Stack direction='horizontal' gap={3}>
+                                            <Badge bg={theme} className='text-background'>HTML</Badge>
+                                            <Badge bg={theme} className='text-background'>CSS</Badge>
+                                            <Badge bg={theme} className='text-background'>JavaScript</Badge>
+                                            <Badge bg={theme} className='text-background'>PHP</Badge>
+                                            <Badge bg={theme} className='text-background'>SQL/MySQL</Badge>
+                                            <Badge bg={theme} className='text-background'>Python</Badge>
+                                        </Stack>
                                     </Col>
                                 </Row>
                             </TabPane>
-                            <TabPane eventKey="#list-stage-3eme-list">
+                            <TabPane eventKey="#list-stage-3eme">
                                 <Row>
-                                    <Col sm={6} className="d-flex justify-content-center">
+                                    <Col sm={4} className="d-flex justify-content-center">
                                         <img src={bullesDogs} alt="Bulles Dogs" className="parcours-image"/>
                                     </Col>
-                                    <Col sm={6} className="d-flex flex-column justify-content-center">
+                                    <Col className="d-flex flex-column justify-content-center">
                                         <h4 className="parcours-name">Stage d'observation de 3<sup>ème</sup></h4>
                                         <p className="fst-italic">
                                             <span className="fw-bold">Semaine du lundi 4 au samedi 9 février 2019</span><br/>
@@ -113,10 +134,13 @@ export function Parcours() {
                     </Col>
                     <Col sm={4}>
                         <ListGroup>
-                            <ListGroupItem action href="#list-stage-dev-web-list" variant="info">
+                            <ListGroupItem action href="#list-stage-but3" variant="info" disabled>
+                                Stage de développeur php
+                            </ListGroupItem>
+                            <ListGroupItem action href="#list-stage-but2" variant="info">
                                 Stage de développeur web
                             </ListGroupItem>
-                            <ListGroupItem action href="#list-stage-3eme-list" variant="info">
+                            <ListGroupItem action href="#list-stage-3eme" variant="info">
                                 Stage d'observation de 3<sup>ème</sup>
                             </ListGroupItem>
                         </ListGroup>
