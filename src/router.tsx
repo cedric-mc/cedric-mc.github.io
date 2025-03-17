fb4d117122f0a45c5408b599273f8b4f0ff15bc3
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-17 14:56:25 +0100

    update router to include AboutMe component and pass theme prop to Parcours; adjust scroll margin for all elements

--- src/router.tsx
+++ src/router.tsx
@@ -6,15 +6,18 @@ import { Skills } from "./pages/Skills";
 import { Projects } from "./pages/Projects";
 import { Header } from "./components/Header";
 
+const theme = localStorage.getItem("theme") || "auto";
+const invertTheme = theme === "light" ? "dark" : "light";
+
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
         children: [
-            { index: true, element: <Header /> },
+            { index: true, element: <><Header /><AboutMe /></> },
             // { index: true, element: <Home /> }, // Accueil avec ton prénom en grand
             { path: "about", element: <AboutMe /> },
-            { path: "parcours", element: <Parcours /> },
+            { path: "parcours", element: <Parcours theme={invertTheme} /> },
             { path: "skills", element: <Skills /> },
             { path: "projects", element: <Projects /> }
         ]
