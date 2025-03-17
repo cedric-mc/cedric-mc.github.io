6cf18f1852c2d8f480f51adedcf5e87029cb9a1c
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-17 14:57:49 +0100

    update: enhance AboutMe component with language flags and clean up content

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col, Row } from "react-bootstrap";
 import '@styles/pages/AboutMe.css';
 import photo from '@assets/photo.jpg';
+import english_flag from '@assets/navbar/eng.png';
+import tamil_flag from '@assets/tamil.png';
 
 export function AboutMe() {
     return (
@@ -13,18 +15,18 @@ export function AboutMe() {
                 <Col>
                     <p className="text-justify">
                         Je suis un étudiant à l'Université de Gustave Eiffel en 3<sup>ème</sup> année du BUT
-                        Informatique. Je suis actuellement en recherche d'un stage dans le domaine du développement
+                        Informatique. {/* Je suis actuellement en recherche d'un stage dans le domaine du développement
                         informatique pour une durée de 12 à 16 semaines à partir du 17 mars 2024. Je suis à la recherche
                         d'une entreprise qui me permettra de mettre en pratique mes connaissances et mes compétences
-                        dans le domaine du développement informatique.
+                        dans le domaine du développement informatique. */}
                     </p>
-                    {/* <p>
+                    <p>
                         Voici mon CV, il est à votre disposition :&nbsp;
                         <a id="cv-btn" className="btn btn-danger heartbeat heartbeat-hover" href="/CV_CEDRIC_MARIYA-CONSTANTINE.pdf" target="_blank">
                             Télécharger&nbsp;<FontAwesomeIcon icon={faDownload}/>
                         </a>
-                    </p> */}
-                    <p>Je maîtrise différentes langues en dehors du français : l'Anglais <img className="p-img" src="img/navbar/eng.png" alt="icon flag english"/> bien entendu et le Tamoul <img className="p-img" src="img/tamil.png" alt="icon flag tamil"/> (langue maternelle).</p>
+                    </p>
+                    <p>Je maîtrise différentes langues en dehors du français : l'Anglais <img className="p-img" src={english_flag} alt="icon flag english"/> bien entendu et le Tamoul <img className="p-img" src={tamil_flag} alt="icon flag tamil"/> (langue maternelle).</p>
                     <p>Je vous invite à me découvrir à travers ce Portfolio, tout en prenant une petite collation !</p>
                     <p>
                         Vous pouvez également me contacter via le formulaire de contact et me suivre sur :{' '}
