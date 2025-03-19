efc94af20f4a16cf85dc60f2172cbb21f05b50ee
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-19 23:14:23 +0100

    update: refactor theme handling in App component for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,21 @@ import { RouterProvider } from 'react-router';
 import { router } from './router';
 
 function App() { //TODO: Augmenter la taille des marges pour les écrans larges
-    useEffect(() => {
-        const savedTheme = localStorage.getItem("theme") || "auto";
-        const body = document.querySelector("body") as HTMLElement;
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "auto";
+    const body = document.querySelector("body") as HTMLElement;
 
-        if (savedTheme === "auto") {
-            const autoTheme = themeBasedOnTime();
-            body.setAttribute("data-theme", "auto");
-            body.setAttribute("data-auto-theme", autoTheme);
-            localStorage.setItem("auto-theme", autoTheme);
-        } else {
-            body.setAttribute("data-theme", savedTheme);
-        }
-    }, [localStorage]);
+    if (savedTheme === "auto") {
+      const autoTheme = themeBasedOnTime();
+      body.setAttribute("data-theme", "auto");
+      body.setAttribute("data-auto-theme", autoTheme);
+      localStorage.setItem("auto-theme", autoTheme);
+    } else {
+      body.setAttribute("data-theme", savedTheme);
+    }
+  }, [localStorage]);
 
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
