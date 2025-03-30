92192e19201204e1117dd146aaf12b5559cbf9f1
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-30 10:43:02 +0200

    fix: redirect to SkillsUniv route based on user status in Skills component

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -25,49 +25,60 @@ import vscode from '@assets/skills/vscode.png';
 import git from '@assets/skills/git.png';
 import github from '@assets/skills/github.png';
 import jetbrains from '@assets/skills/jetbrains.png';
+import { useNavigate } from 'react-router';
+import { useEtudes } from '../hooks/useEtudes';
+import { useEffect } from 'react';
 
 export function Skills() {
-    return (
-        <section id="skills">
-            <h2 className="title-section">Mes Compétences</h2>
-                <p>Voici les langages informatiques, frameworks, bibliothèques, logiciels et outils que je maîtrise :</p>
-                <div>
-                    <h3 className="subtitle-section">Langages informatiques :</h3>
-                    <ul className="skills-list">
-                        <li><img className="spinner" src={html} alt=""/>&nbsp;HTML</li>
-                        <li><img className="spinner" src={css} alt=""/>&nbsp;CSS</li>
-                        <li><img className="spinner" src={js} alt=""/>&nbsp;JavaScript</li>
-                        <li><img className="spinner" src={ts} alt=""/>&nbsp;TypeScript</li>
-                        <li><img className="spinner" src={php} alt=""/>&nbsp;PHP</li>
-                        <li><img className="spinner" src={python} alt=""/>&nbsp;Python</li>
-                        <li><img className="spinner" src={java} alt=""/>&nbsp;Java</li>
-                        <li><img className="spinner" src={c} alt=""/>&nbsp;C</li>
-                        <li><img className="spinner" src={sql} alt=""/>&nbsp;SQL</li>
-                        <li><img className="spinner" src={xml} alt=""/>&nbsp;XML</li>
-                        <li><img className="spinner" src={json} alt=""/>&nbsp;JSON</li>
-                        <li><img className="spinner" src={markdown} alt=""/>&nbsp;Markdown</li>
-                        {/* <li><img className="spinner" src={latex} alt=""/>&nbsp;LaTeX</li> */}
-                        <li><img className="spinner" src={shell} alt=""/>&nbsp;Shell</li>
-                    </ul>
-                    <h3 className="subtitle-section">Frameworks et bibliothèques :</h3>
-                    <ul className="skills-list">
-                        <li><img className="spinner" src={bootstrap} alt=""/>&nbsp;Bootstrap</li>
-                        <li><img className="spinner" src={jquery} alt=""/>&nbsp;jQuery</li>
-                        <li><img className="spinner" src={react} alt=""/>&nbsp;React</li>
-                        <li><img className="spinner" src={nodejs} alt=""/>&nbsp;Node.js</li>
-                        <li><img className="spinner" src={junit} alt=""/>&nbsp;JUnit</li>
-                        <li><img className="spinner" src={maven} alt=""/>&nbsp;Maven</li>
-                        <li><img className="spinner" src={mysql} alt=""/>&nbsp;MySQL</li>
-                        <li><img className="spinner" src={postgresql} alt=""/>&nbsp;PostgreSQL</li>
-                    </ul>
-                    <h3 className="subtitle-section">Logiciels et outils :</h3>
-                    <ul className="skills-list">
-                        <li><img className="spinner" src={vscode} alt=""/>&nbsp;Visual Studio Code</li>
-                        <li><img className="spinner" src={git} alt=""/>&nbsp;Git</li>
-                        <li><img className="spinner" src={github} alt=""/>&nbsp;GitHub</li>
-                        <li><img className="spinner" src={jetbrains} alt=""/>&nbsp;JetBrains</li>
-                    </ul>
-                </div>
-        </section>
-    );
+  const navigate = useNavigate();
+  const isEtudes = useEtudes();
+  useEffect(() => {
+    if (isEtudes) {
+      navigate('/skills-univ');
+    }
+  }, [isEtudes, navigate]);
+
+  return (
+    <section id="skills">
+      <h2 className="title-section">Mes Compétences</h2>
+      <p>Voici les langages informatiques, frameworks, bibliothèques, logiciels et outils que je maîtrise :</p>
+      <div>
+        <h3 className="subtitle-section">Langages informatiques :</h3>
+        <ul className="skills-list">
+          <li><img className="spinner" src={html} alt="" />&nbsp;HTML</li>
+          <li><img className="spinner" src={css} alt="" />&nbsp;CSS</li>
+          <li><img className="spinner" src={js} alt="" />&nbsp;JavaScript</li>
+          <li><img className="spinner" src={ts} alt="" />&nbsp;TypeScript</li>
+          <li><img className="spinner" src={php} alt="" />&nbsp;PHP</li>
+          <li><img className="spinner" src={python} alt="" />&nbsp;Python</li>
+          <li><img className="spinner" src={java} alt="" />&nbsp;Java</li>
+          <li><img className="spinner" src={c} alt="" />&nbsp;C</li>
+          <li><img className="spinner" src={sql} alt="" />&nbsp;SQL</li>
+          <li><img className="spinner" src={xml} alt="" />&nbsp;XML</li>
+          <li><img className="spinner" src={json} alt="" />&nbsp;JSON</li>
+          <li><img className="spinner" src={markdown} alt="" />&nbsp;Markdown</li>
+          {/* <li><img className="spinner" src={latex} alt=""/>&nbsp;LaTeX</li> */}
+          <li><img className="spinner" src={shell} alt="" />&nbsp;Shell</li>
+        </ul>
+        <h3 className="subtitle-section">Frameworks et bibliothèques :</h3>
+        <ul className="skills-list">
+          <li><img className="spinner" src={bootstrap} alt="" />&nbsp;Bootstrap</li>
+          <li><img className="spinner" src={jquery} alt="" />&nbsp;jQuery</li>
+          <li><img className="spinner" src={react} alt="" />&nbsp;React</li>
+          <li><img className="spinner" src={nodejs} alt="" />&nbsp;Node.js</li>
+          <li><img className="spinner" src={junit} alt="" />&nbsp;JUnit</li>
+          <li><img className="spinner" src={maven} alt="" />&nbsp;Maven</li>
+          <li><img className="spinner" src={mysql} alt="" />&nbsp;MySQL</li>
+          <li><img className="spinner" src={postgresql} alt="" />&nbsp;PostgreSQL</li>
+        </ul>
+        <h3 className="subtitle-section">Logiciels et outils :</h3>
+        <ul className="skills-list">
+          <li><img className="spinner" src={vscode} alt="" />&nbsp;Visual Studio Code</li>
+          <li><img className="spinner" src={git} alt="" />&nbsp;Git</li>
+          <li><img className="spinner" src={github} alt="" />&nbsp;GitHub</li>
+          <li><img className="spinner" src={jetbrains} alt="" />&nbsp;JetBrains</li>
+        </ul>
+      </div>
+    </section>
+  );
 };
