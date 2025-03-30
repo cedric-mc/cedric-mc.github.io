b0e402daaa2191eacc53673470a759a5a4c951cd
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-30 22:44:02 +0200

    refactor: update ProjectStudies component to use Project type and enhance modal content display

--- src/components/Projects/ProjectStudies.tsx
+++ src/components/Projects/ProjectStudies.tsx
@@ -1,11 +1,10 @@
 import { Modal, ModalBody, ModalFooter, ModalHeader } from "react-bootstrap";
+import { Project } from "../../types/types";
 
 interface ProjectStudiesProps {
   show: boolean;
   onClose: () => void;
-  project: {
-    title: string;
-  };
+  project: Project;
   theme: string;
 }
 
@@ -13,12 +12,20 @@ export function ProjectStudies({ show, onClose, project, theme }: ProjectStudies
   return (
     <Modal show={show} onHide={onClose} data-bs-theme={theme} size="xl" backdrop="static">
       <ModalHeader closeButton>
-        <Modal.Title>{project.title}</Modal.Title>
+        <Modal.Title>{project.header}</Modal.Title>
       </ModalHeader>
       <ModalBody>
+        <div className="mb-3">
+          <p>{project.text}</p>
+          <hr />
+          <h5>Compétences acquises</h5>
+          <ul>
+            {project.etudes}
+          </ul>
+        </div>
       </ModalBody>
       <ModalFooter>
       </ModalFooter>
-    </Modal>
+    </Modal >
   )
 }
