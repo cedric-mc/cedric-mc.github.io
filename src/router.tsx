4697684c296cda0616a8748f7a14109bd359b84c
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-30 10:42:53 +0200

    fix: add SkillsUniv route to the router configuration

--- src/router.tsx
+++ src/router.tsx
@@ -4,20 +4,22 @@ import { Parcours } from "./pages/Parcours";
 import { Skills } from "./pages/Skills";
 import { Projects } from "./pages/Projects";
 import { Home } from "./pages/Home";
+import { SkillsUniv } from "./pages/SkillsUniv";
 
 const theme = localStorage.getItem("theme") || "auto";
 // const finalTheme = theme === "auto" ? localStorage.getItem("auto-theme") || "light" : theme;
 const invertTheme = theme === "light" ? "dark" : "light";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            { index: true, element: <Home /> },
-            { path: "parcours", element: <Parcours theme={invertTheme} /> },
-            { path: "skills", element: <Skills /> },
-            { path: "projects", element: <Projects /> }
-        ]
-    }
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "parcours", element: <Parcours theme={invertTheme} /> },
+      { path: "skills", element: <Skills /> },
+      { path: "skills-univ", element: <SkillsUniv /> },
+      { path: "projects", element: <Projects /> }
+    ]
+  }
 ]);
