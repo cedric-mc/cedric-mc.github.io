d420bb015a6dbe7ce900fd34db643bb24f472cfb
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-30 11:22:31 +0200

    refactor: enhance useEtudes hook to manage state based on URL parameters and localStorage

--- src/hooks/useEtudes.ts
+++ src/hooks/useEtudes.ts
@@ -1,15 +1,28 @@
 import { useState, useEffect } from 'react';
 
 export const useEtudes = (): boolean => {
-    const [isEtudes, setIsEtudes] = useState<boolean>(false); // Par défaut, `isEtudes` est `false`
+  const [isEtudes, setIsEtudes] = useState<boolean>(false); // Par défaut, `isEtudes` est `false`
 
-    useEffect(() => {
-        const queryParams = new URLSearchParams(window.location.search);
-        const etudes = queryParams.get('e');
-        if (etudes === '1') {
-            setIsEtudes(true); // Active `isEtudes` si la valeur de `e` est "1"
-        }
-    }, []);
+  useEffect(() => {
+    // Je veux que `isEtudes` soit `true` si l'URL contient "e=1" ou que isEtudes est dans le localStorage
+    const urlParams = new URLSearchParams(window.location.search);
+    const etudesParam = urlParams.get('e');
+    const etudesStorage = localStorage.getItem('isEtudes');
+    const etudes = etudesParam === '1' || etudesStorage === 'true';
+    setIsEtudes(etudes);
+    if (etudesParam === '0') {
+      setIsEtudes(false);
+      localStorage.setItem('isEtudes', 'false');
+      return;
+    }
+    // Si l'URL contient "e=1", je veux que isEtudes soit `true` et que le localStorage soit mis à jour
+    if (etudesParam === '1' || etudesStorage === 'true') {
+      localStorage.setItem('isEtudes', 'true');
+    } else { // Sinon, je veux que isEtudes soit `false` et que le localStorage soit mis à jour
+      setIsEtudes(false);
+      localStorage.setItem('isEtudes', 'false');
+    }
+  }, [window.location.search]); // Je veux que l'effet soit exécuté à chaque fois que l'URL change
 
-    return isEtudes;
+  return isEtudes;
 };
