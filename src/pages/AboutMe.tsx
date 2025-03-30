c775149cf4b2d6a8f94f52151bb7eda8ccf43423
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-30 10:42:58 +0200

    fix: enhance AboutMe component text for clarity and engagement

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -15,7 +15,8 @@ export function AboutMe() {
                 <Col>
                     <p className="text-justify">
                         Je suis un étudiant à l'Université de Gustave Eiffel en 3<sup>ème</sup> année du BUT
-                        Informatique. {/* Je suis actuellement en recherche d'un stage dans le domaine du développement
+                        Informatique. Je suis une personne passionnée par l'informatique et le développement en particulier.
+                        {/* Je suis actuellement en recherche d'un stage dans le domaine du développement
                         informatique pour une durée de 12 à 16 semaines à partir du 17 mars 2024. Je suis à la recherche
                         d'une entreprise qui me permettra de mettre en pratique mes connaissances et mes compétences
                         dans le domaine du développement informatique. */}
@@ -29,7 +30,7 @@ export function AboutMe() {
                     <p>Je maîtrise différentes langues en dehors du français : l'Anglais <img className="p-img" src={english_flag} alt="icon flag english"/> bien entendu et le Tamoul <img className="p-img" src={tamil_flag} alt="icon flag tamil"/> (langue maternelle).</p>
                     <p>Je vous invite à me découvrir à travers ce Portfolio, tout en prenant une petite collation !</p>
                     <p>
-                        Vous pouvez également me contacter via le formulaire de contact et me suivre sur :{' '}
+                        Vous pouvez également me contacter via le formulaire de contact tout en bas de chaque page et me suivre sur :{' '}
                         <a href="https://www.linkedin.com/in/cédric-mariya-constantine-a3a52325a/" target="_blank" rel="noreferrer" className="link-body link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover social-link">
                             <FontAwesomeIcon icon={faLinkedin} className="spinner"/>{' '}LinkedIn
                         </a> et{' '}
